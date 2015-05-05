26d1735caa74f14871fd5b838b951a7962068572
kosyk <kosyk@kosyk-VirtualBox.(none)>
2015-05-05 14:40:18 +0200

    Próba wczytywania broadcastu. Zabezpieczenie przed pustą listą użytkowników.

--- SKP/src/ClientThread.java
+++ SKP/src/ClientThread.java
@@ -1,7 +1,10 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
 import java.net.SocketTimeoutException;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClientThread  implements Runnable
@@ -10,11 +13,15 @@ public void run() {
 	
     try {
     	//Wysyłanie na broadcast
+    	List<InterfaceAddress> interfaceAddress = NetworkInterface.getNetworkInterfaces().nextElement().getInterfaceAddresses();
+    	InetAddress broadcast = interfaceAddress.get(1).getBroadcast();
+    	
+    	
     	DatagramSocket socket = new DatagramSocket();
     	String message = "Witam w Rumbie";
     	byte[] stringContents = message.getBytes("utf8");
-        InetAddress serverAddress = InetAddress.getByName("192.168.20.255"); 
-
+        InetAddress serverAddress = broadcast; 
+        
 		DatagramPacket sent = new DatagramPacket(
 				stringContents, stringContents.length, serverAddress, Config.PORT);
 		socket.send(sent);
@@ -30,8 +37,6 @@ public void run() {
 				    String name =
 		                    new String(recv.getData(), 0, length, "utf8");
 				    	if (!name.equals(System.getProperty("user.name"))){
-				    		System.out.println("Serwer otrzymał wiadomość i nazywa się:"+name);
-						    System.out.println("Jego adres: "+recv.getAddress());
 				    		Program.usersList.add(new User(name, recv.getAddress()));
 				    	}
 				    		
@@ -41,40 +46,49 @@ public void run() {
 				}
 			}
 			
-			System.out.println("Wszystkich użytkowników już mam, wychodzę z while'a...");
-			System.out.println("Użytkownicy na liście");
-			for (int j=0; j<Program.usersList.size(); j++){
-				System.out.println("Nr"+(j+1));
-				Program.usersList.get(j).printUser();
-			}
-			
-			System.out.println("Wybierz użytkownika:");
-			int choice;
-			Scanner in=new Scanner(System.in);
-			choice=in.nextInt()-1;
+			System.out.println("Przeszukano sieć");
 			
 			
-			//prośba o listę katalogów/plików
-			DatagramSocket fileSocket = new DatagramSocket(Config.FILEPORT);
-			socket.setSoTimeout(1010);
-			byte[] request = "Lista".getBytes("utf8"); 
-
-			DatagramPacket fileRequest = new DatagramPacket(
-					request, request.length, Program.usersList.get(choice).getUserAddres(), Config.FILEPORT);
-			fileSocket.send(fileRequest);
-			
-			//oczekiwanie na listę katalogów
-			while(true){
-				try{
-					fileSocket.receive(recv);
-				}catch (SocketTimeoutException ste){
-					break;
+			if (!Program.usersList.isEmpty()){
+				
+				System.out.println("Użytkownicy na liście");
+				for (int j=0; j<Program.usersList.size(); j++){
+					System.out.println("Nr"+(j+1));
+					Program.usersList.get(j).printUser();
 				}
+				
+				System.out.println("Wybierz użytkownika:");
+				int choice;
+				Scanner in=new Scanner(System.in);
+				choice=in.nextInt()-1;
+				
+				
+				//prośba o listę katalogów/plików
+				DatagramSocket fileSocket = new DatagramSocket(Config.FILEPORT);
+				socket.setSoTimeout(1010);
+				byte[] request = "Lista".getBytes("utf8"); 
+
+				DatagramPacket fileRequest = new DatagramPacket(
+						request, request.length, Program.usersList.get(choice).getUserAddres(), Config.FILEPORT);
+				fileSocket.send(fileRequest);
+				
+				//oczekiwanie na listę katalogów
+				while(true){
+					try{
+						fileSocket.receive(recv);
+					}catch (SocketTimeoutException ste){
+						break;
+					}
+				}
+				
+				//wybór katalogu/pliku
+				
+				//odbiór pliku
+			}
+			else {
+				System.out.println("Brak podłączonych użytkowników");
 			}
 			
-			//wybór katalogu/pliku
-			
-			//odbiór pliku
 			
 	} catch (Exception e) {
 		e.printStackTrace();
