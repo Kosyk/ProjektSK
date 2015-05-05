916166896fd28da4cacfb9cf41dd34a6521a85e7
kosyk <kosyk@kosyk-VirtualBox.(none)>
2015-05-05 15:08:40 +0200

    Zmiana potru

--- SKP/src/Config.java
+++ SKP/src/Config.java
@@ -1,5 +1,5 @@
 public class Config {
     public static final int PORT = 9000;
-    public static final int FILEPORT = 8500;
+    public static final int FILEPORT = 9010;
     public static final int BUFFER_SIZE = 1024;
 }
